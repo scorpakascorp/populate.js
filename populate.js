631d934f090323136b945a65ed2f96f7a8aa4b9b
scorpakascorp <gsv7@rambler.ru>
2020-07-08 12:46:07 +0300

    Update populate.js

--- populate.js
+++ populate.js
@@ -1,4 +1,3 @@
-
 /**
  * Populate form fields from a JSON object.
  *
@@ -7,75 +6,82 @@
  * @param basename string Optional basename which is added to `name` attributes
  */
 function populate(form, data, basename) {
-	for (var key in data) {
-		if (! data.hasOwnProperty(key)) {
-			continue;
-		}
+  for (var key in data) {
+    if (!data.hasOwnProperty(key)) {
+      continue;
+    }
 
-		var name = key;
-		var value = data[key];
+    var name = key;
+    var value = data[key];
 
-        if ('undefined' === typeof value) {
-            value = '';
+    if ('undefined' === typeof value) {
+      value = '';
+    }
+
+    if (null === value) {
+      value = '';
+    }
+
+    // handle array name attributes
+    if (typeof basename !== 'undefined') {
+      name = basename + '[' + key + ']';
+    }
+
+    if (value.constructor === Array) {
+      name += '[]';
+    } else if (typeof value == 'object') {
+      populate(form, value, name);
+      continue;
+    }
+
+    // only proceed if element is set
+    var element = form.elements.namedItem(name);
+    if (!element) {
+      continue;
+    }
+
+    var type = element.type || element[0].type;
+
+    switch (type) {
+      default:
+        element.value = value;
+        break;
+
+      case 'radio':
+        for (var j = 0; j < element.length; j++) {
+          element[j].checked = String(value) === String(element[j].value);
         }
+        break;
 
-        if (null === value) {
-            value = '';
+      case 'checkbox':
+        var values = value.constructor == Array ? value : [value];
+
+        for (var k = 0; k < element.length; k++) {
+          element[k].checked = values.indexOf(element[k].value) > -1;
         }
+        break;
 
-		// handle array name attributes
-		if (typeof(basename) !== "undefined") {
-			name = basename + "[" + key + "]";
-		}
+      case 'select-multiple':
+        var values = value.constructor == Array ? value : [value];
 
-		if (value.constructor === Array) {
-			name += '[]';
-		} else if(typeof value == "object") {
-			populate(form, value, name);
-			continue;
-		}
+        for (var m = 0; m < element.options.length; m++) {
+          element.options[m].selected =
+            values.indexOf(element.options[m].value) > -1;
+        }
+        break;
 
-		// only proceed if element is set
-		var element = form.elements.namedItem(name);
-		if (! element) {
-			continue;
-		}
+      case 'select':
+      case 'select-one':
+        element.value = value.toString() || value;
+        break;
 
-		var type = element.type || element[0].type;
-
-		switch(type ) {
-			default:
-				element.value = value;
-				break;
-
-			case 'radio':
-			case 'checkbox':
-				for (var j=0; j < element.length; j++) {
-					element[j].checked = (String(value) === String(element[j].value));
-				}
-				break;
-
-			case 'select-multiple':
-				var values = value.constructor == Array ? value : [value];
-
-				for(var k = 0; k < element.options.length; k++) {
-					element.options[k].selected = (values.indexOf(element.options[k].value) > -1 );
-				}
-				break;
-
-			case 'select':
-			case 'select-one':
-				element.value = value.toString() || value;
-				break;
-
-			case 'date':
-      			element.value = new Date(value).toISOString().split('T')[0];	
-				break;
-		}
-
-	}
-};
+      case 'date':
+        element.value = new Date(value).toISOString().split('T')[0];
+        break;
+    }
+  }
+}
 
 if (typeof module !== 'undefined' && module.exports) {
-	module.exports = populate;
-} 
+  module.exports = populate;
+}
